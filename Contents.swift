5eeba0dd3875c7c9d1d4ac431a4edefab423d17a
Kaiba Murphy <kautmnm121@gmail.com>
2019-07-09 09:47:48 -0700

    Initial Commit

---
+++ Contents.swift
@@ -0,0 +1,16 @@
+
+
+import UIKit
+
+//intentions: to create aprgram that will check if a number is odd or even
+func numberchecker ( number : Int) {
+    if (number % 2 == 0){//if statement to check for even numbers
+        print("even")
+    }
+    else{//if statemnet for odd numbers
+        print("odd")
+    }
+}
+
+numberchecker ( number : 6)
+
